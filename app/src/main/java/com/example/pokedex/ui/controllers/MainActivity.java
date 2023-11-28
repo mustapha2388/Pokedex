ad5c00b3ea850d97bd8438453dbe005cf179f239
mouss <mouss2388@gmail.com>
2023-11-28 19:21:57 +0100

    Create toolbar
    +Add toolbar with  it style
    + Add sort button
    + Add recyclerview

--- app/src/main/java/com/example/pokedex/ui/controllers/MainActivity.java
+++ app/src/main/java/com/example/pokedex/ui/controllers/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.pokedex.ui.controllers;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pokedex.R;
 import com.example.pokedex.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+
+        hideStatusBar();
+        setSupportActionBar(binding.toolbar.getRoot());
+        ///Add back button
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("");
+        }
+    }
+
+    private void hideStatusBar() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
